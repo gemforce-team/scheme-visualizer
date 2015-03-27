ab7c956bf4abf61259c574d851c7b14133495d04
tulamide <tulamide@users.noreply.github.com>
2015-03-27 20:42:30 +0100

    Faster algorithms, added zoom and positioning.
    right click'n'drag for zooming, left click'n'drag for positioning
    right or left doubleclick resets zoom or position

--- graphics.rb
+++ graphics.rb
@@ -10,92 +10,139 @@ def init
 	@grey = Color.new 255, 130
 	@brush = Brush.new
 	@grade = 1
-	@steps = 1
-	#setViewSize 0, [0, 5, 120, 60]
+	@deepest = 0
+	@nodelist = []
+	@zoom = 1.0
+	@offset = [0.0, 0.0]
+	@buttons = [[],[]]
 	redraw
 end
 
 def event i, v
 	if i == "source"
-		@tree = Fork.new
+		@tree = Fork.new 0,0,true
 		@tree.me = v
-		n = -1
-		row = @tree.last
-		until n != -1
-			n = amount @tree, row - 1
-			row -=1
-		end
-		@steps = n
-		watch "steps", n
+		@tree.value = @tree.calc
+		@deepest = @tree.last
+		@nodelist = @tree.flatten
+		@nodelist.flatten!
+		watch @deepest
 		redraw
 	end
 end
 
-def amount tree, row
-	result = -1
-	nodes = 2**row
-	r = nodes * 2 - 2
-	l = nodes - 1
-	r.downto(l) do |n|
-		if tree.node(n) != nil
-			result = 1 + n - l
-			if result < 2**(row - 1)
-				result = -1
-			end
-			break
-		end
-	end
-	return result
-end
-
 def draw v
-	#return if @brush == nil
 	@brush.setColor @background
 	v.drawRectangle @brush, [0, 0, v.width, v.height]
-	return if @tree == nil or @steps == nil
+	return if @tree == nil or @nodelist.length == 0
+	return if @tree.me == ""
 	
-	deepest = @tree.last
-	nodes = 2**deepest
-	hori = v.width / nodes
-	vert = v.height / deepest
-	obj = nil
-	
-	deepest.downto(0) do |row|
-		low = 2**row - 1
-		high = low + @steps
-		width = 2**(deepest - row) * hori
-		
-		(low..high).each do |id|
-			x = (id - low) * width + (width * 0.5)
-			obj = @tree.node(id)
-			if obj != nil
-				x = (id - low) * width + (width * 0.5)
-				y = hori * 0.5 + row * (vert - hori / deepest)
-				grade = obj.value
-				gem = nil
-				@brush.setColor case obj.me 
-				when "b" 
-					@black
-				when "r"
-					@red
-				when "o", "m"
-					@orange
-				when "y", "k"
-					@yellow
-				else
-					@grey
-				end
-				v.drawPath @brush, method("g" + grade.to_s).call([x, y], hori * 0.5)
-				
-				if row > 0
-					if id.even?
-						v.drawLine Pen.new(@grey, 0.125), [x, y - hori * 0.75], [x - width * 0.5, (row-1) * vert + hori * 0.75]
-					else
-						v.drawLine Pen.new(@grey, 0.125), [x, y - hori * 0.75], [x + width * 0.5, (row-1) * vert + hori * 0.75]
-					end
-				end
-				
+	radius = 5 * @zoom
+	center = v.width * 0.5 + @offset[0]
+	height = radius * 3
+	line_width = radius * 0.125
+	@nodelist.each do |node|
+		interval = 2**(@deepest - node.layer + 1)
+		offset = 0
+		if node.layer == 0
+			interval = 0
+			offset = 0
+		else
+			1.upto(node.layer) do |n|
+				offset += 2**(@deepest - n)
 			end
+			offset *= radius * -1
 		end
+
+		node.x, node.y = center + offset + node.id * (interval * radius), @offset[1] + radius + 0.125 + node.layer * height
+		
+		if node.from != nil
+			dx = node.from.x - node.x
+			dy = node.from.y - node.y
+			angle = Math.atan2(dy, dx)
+			x1, y1 = node.x + Math.cos(angle) * radius, node.y + Math.sin(angle) * radius
+			dx = node.x - node.from.x
+			dy = node.y - node.from.y
+			angle = Math.atan2(dy, dx)
+			x2, y2 = node.from.x + Math.cos(angle) * radius, node.from.y + Math.sin(angle) * radius
+			
+			pen = Pen.new @grey, line_width
+			pen.setStartCap "roundanchor"
+			pen.setEndCap "round"
+			pen.setLineJoin "round"
+			#v.drawLine pen, [x2, y2], [x1, y1]
+			v.drawLine pen, [node.x, node.y - radius * 1.25], [x2, y2]
+		end
+			
+		p = method("g" + node.value.to_s).call([node.x, node.y], [0.1875, radius].max)
+		
+		@brush.setColor case node.me
+			when "b"
+				@black
+			when "o", "m"
+				@orange
+			when "r"
+				@red
+			when "y", "k"
+				@yellow
+			else
+				@grey
+			end
+		v.drawPath @brush, p
 	end
+end
+
+def distance a, b
+	dx = b[0] - a[0]
+	dy = b[1] - a[1]
+	return Math.sqrt(dx**2 + dy**2)
+end
+
+def isInMousePoint x, y
+	true
+end
+
+def mouseLDouble
+	@offset[0, 1] = 0.0, 0.0
+	redraw
+end
+
+def mouseRDouble
+	@zoom = 1
+	redraw
+end
+
+def mouseLDown x, y
+	@buttons[0][0] = true
+	@buttons[0][1, 2] = x, y
+	captureMouse
+end
+
+def mouseRDown x, y
+	@buttons[1][0] = true
+	@buttons[1][1] = y
+	captureMouse
+end
+
+def mouseMoveCaptured x, y
+	if @buttons[1][0]
+		@zoom += ((y - @buttons[1][1]) * 0.05) * @zoom
+		@buttons[1][1] = y
+		redraw
+	elsif @buttons[0][0]
+		@offset[0] += (x - @buttons[0][1]) * 0.95
+		@offset[1] += (y - @buttons[0][2]) * 0.95
+		@buttons[0][1, 2] = x, y
+		redraw
+	end
+end
+
+def mouseLUp x, y
+	@buttons[0][0] = false
+	releaseMouse
+end
+
+def mouseRUp x, y
+	@buttons[1][0] = false
+	releaseMouse
 end
